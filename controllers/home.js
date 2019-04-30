ba42f7096ed70ec932db6c46dbaabdae97b489da
Tchana <valdotnv@gmail.com>
2019-04-30 15:39:01 +0100

    Valdo| create viewModel in home controller

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,43 @@
 module.exports = {
     index: function(req, res){
-        res.render('index');
+        const viewModel = {
+            images: [
+                {
+                    uniqueId: 1,
+                    title: 'sample image 1',
+                    description: '',
+                    filename: 'sample1.jpg',
+                    views: 0,
+                    likes: 0,
+                    timestamp: Date.now
+                },{
+                    uniqueId: 2,
+                    title: 'sample image 2',
+                    description: '',
+                    filename: 'sample2.jpg',
+                    views: 0,
+                    likes: 0,
+                    timestamp: Date.now
+                },{
+                    uniqueId: 3,
+                    title: 'sample image 3',
+                    description: '',
+                    filename: 'sample3.jpg',
+                    views: 0,
+                    likes: 0,
+                    timestamp: Date.now
+                },{
+                    uniqueId: 4,
+                    title: 'sample image 4',
+                    description: '',
+                    filename: 'sample4.jpg',
+                    views: 0,
+                    likes: 0,
+                    timestamp: Date.now
+                }
+
+            ]
+        };
+        res.render('index', viewModel);       
     }
 };
